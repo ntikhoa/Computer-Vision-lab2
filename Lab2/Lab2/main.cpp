a7e7a69e58f64f58d7fe3391d864f82ad56841ff
Khoa <ntikhoa123@gmail.com>
2021-11-15 16:01:19 +0700

    test video capture

--- Lab2/Lab2/main.cpp
+++ Lab2/Lab2/main.cpp
@@ -5,8 +5,8 @@
 #include "SiftHarris.h"
 #include "SiftDoG.h"
 #include "SiftBlob.h"
+#include <opencv2/videoio.hpp>
 
-//void DoGdetectorWithSiftDescriptor(String fileName1, String fileName2);
 
 int main(int argc, char* argv[])
 {
@@ -17,5 +17,24 @@ int main(int argc, char* argv[])
     //harrisDetectorWithSiftDescriptor("text_in_scene.jpg", "headpat_rem.jpg");
     //DoGdetectorWithSiftDescriptor("text_in_scene.jpg", "headpat_rem.jpg");
     //blobDetectorWithSiftDescriptor("headpat_rem_croped.jpg", "headpat_rem.jpg");
+
+    
+    Mat frame;
+    VideoCapture cap;
+    int deviceID = 0;             // 0 = open default camera
+    int apiID = cv::CAP_ANY;      // 0 = autodetect default API
+    cap.open(deviceID, apiID);
+    if (!cap.isOpened()) {
+        cerr << "ERROR! Unable to open camera\n";
+        return -1;
+    }
+    namedWindow("Live");
+    while(getWindowProperty("Live", WND_PROP_VISIBLE) > 0)
+    {
+        cap.read(frame);
+        imshow("Live", frame);
+        waitKey(5);
+    }
+    
     return 0;
 }
